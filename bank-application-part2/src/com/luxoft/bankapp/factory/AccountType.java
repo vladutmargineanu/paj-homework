4276af1f2736b4796ac633a0ab4c11012f580b25
vladutmargineanu <vladut.margineanu@gmail.com>
2023-01-07 18:18:45 +0200

    complete enum accountType

--- bank-application-part2/src/com/luxoft/bankapp/factory/AccountType.java
+++ bank-application-part2/src/com/luxoft/bankapp/factory/AccountType.java
@@ -1,6 +1,23 @@
 package com.luxoft.bankapp.factory;
 
 public enum AccountType {
-    CHECKING,
-    SAVING
+    CHECKING("checking"),
+    SAVING("saving");
+
+    private final String accountType;
+
+    AccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountType{" +
+                "accountType='" + accountType + '\'' +
+                '}';
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
 }
